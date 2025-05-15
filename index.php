e791443e39f453a6e5e79a860e582b37f6516f43
Ma Angelica A Arevalo <76656116+MAAArevalo@users.noreply.github.com>
2025-05-16 05:19:30 +0800

    Database

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
+require 'Database.php';
 require 'functions.php';
 
 require 'routes.php';
+
+
+
+//$db = new Database();
+
+// $args = [
+//     "product_quantity" => 5
+// ];
+// $products = $db->get_products($args);
+// echo '<pre>';
+// print_r($products);
+// echo '</pre>';
